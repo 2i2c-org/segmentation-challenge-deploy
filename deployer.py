2cfef1901adb853126e065bade62be4498a46636
Georgiana Dolocan <georgiana.dolocan@gmail.com>
2024-08-30 17:03:31 +0300

    Setup support chart deploy

--- deployer.py
+++ deployer.py
@@ -1,52 +1,90 @@
 import argparse
+import subprocess
+from contextlib import contextmanager
 from pathlib import Path
 
-import subprocess
+
+@contextmanager
+def auth(
+    cluster="openorganelle",
+    location="us-west2-a",
+    project="segmentation-challenge",
+):
+    subprocess.check_call(
+        [
+            "gcloud",
+            "container",
+            "clusters",
+            f"--zone={location}",
+            f"--project={project}",
+            "get-credentials",
+            cluster,
+        ]
+    )
+    yield
+
 
 def deploy(type, namespace, values_file, debug, dry_run):
-  print(values_file)
-  if type == "support":
-    helm_chart = Path(__file__).parent.joinpath("helm-charts/support")
-  elif type == "app":
-    helm_chart = Path(__file__).parent.joinpath("helm-charts/app")
-  else:
-    raise ValueError(f"Unknown type: {type}")
+    print(values_file)
+    if type == "support":
+        helm_chart = Path(__file__).parent.joinpath("helm-charts/support")
+    elif type == "app":
+        helm_chart = Path(__file__).parent.joinpath("helm-charts/app")
+    else:
+        raise ValueError(f"Unknown type: {type}")
 
-  cmd = [
-    "helm",
-    "upgrade",
-    "--install",
-    "--create-namespace",
-    "--wait",
-    f"--namespace={namespace}",
-    namespace,
-    helm_chart
-  ]
+    cmd = [
+        "helm",
+        "upgrade",
+        "--install",
+        "--create-namespace",
+        "--wait",
+        f"--namespace={namespace}",
+        namespace,
+        helm_chart,
+    ]
 
-  if values_file:
-    values_file = helm_chart.joinpath(values_file)
-    cmd.append(f"--values={values_file}")
+    if values_file:
+        values_file = helm_chart.joinpath(values_file)
+        cmd.append(f"--values={values_file}")
 
-  if dry_run:
-    cmd.append("--dry-run")
+    if dry_run:
+        cmd.append("--dry-run")
 
-  if debug:
-    cmd.append("--debug")
+    if debug:
+        cmd.append("--debug")
 
-  print(f"Running {' '.join([str(c) for c in cmd])}")
-  subprocess.check_call(cmd)
+    print(f"Running {' '.join([str(c) for c in cmd])}")
+    subprocess.check_call(cmd)
+
 
 def main():
-  parser = argparse.ArgumentParser(description='Deploy script')
-  parser.add_argument('type', type=str, help='Type of deployment. Choose from support/app!')
-  parser.add_argument('--namespace', type=str, help='Namespace to deploy to')
-  parser.add_argument('--values_file', type=str, help='Name of the values file', default=None)
-  parser.add_argument('--dry-run', action='store_true', help='Perform a dry run')
-  parser.add_argument('--debug', action='store_true', help='Enable debug mode')
+    parser = argparse.ArgumentParser(description="Deploy script")
+    parser.add_argument(
+        "type", type=str, help="Type of deployment. Choose from support/app!"
+    )
+    parser.add_argument("--namespace", type=str, help="Namespace to deploy to")
+    parser.add_argument(
+        "--values_file", type=str, help="Name of the values file", default=None
+    )
+    parser.add_argument(
+        "--dry-run", action="store_true", help="Perform a dry run"
+    )
+    parser.add_argument(
+        "--debug", action="store_true", help="Enable debug mode"
+    )
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  deploy(args.type, args.namespace, args.values_file, args.debug, args.dry_run)
+    with auth():
+        deploy(
+            args.type,
+            args.namespace,
+            args.values_file,
+            args.debug,
+            args.dry_run,
+        )
+
 
 if __name__ == "__main__":
     main()
